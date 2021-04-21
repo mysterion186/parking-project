4992cb82c1314c3954f85e90bf968540f0874d6f
Mysterion186 <kowsikan@MacBook-Pro-de-Julien.local>
2021-04-21 21:47:27 +0200

    version avec config

--- parking.py
+++ parking.py
@@ -10,6 +10,13 @@ on aura que les bordures qui auront une couleur sinon toutes les places seront c
 # - il faut 20 pixel d'écart selon y pour avoir de "jolie bouton"
 # - il faut 55 pixel d'écart selon x pour avoir de "jolie bouton"
 
+"""
+Configuration : 
+niveau 0 : 2 allées de 5 places et 4 places 
+niveau 1 : 2 allées de 5 places et 4 places 
+niveau 2 : 2 allées de 5 places et 4 places 
+"""
+
 ##############################################################################################################################
 # Déclaration des variables                                                                                                  #
 ##############################################################################################################################
@@ -28,7 +35,7 @@ class Parking :
         self.x = x #coordonnée x du bouton
         self.y = y #coordonnée y du bouton
         self.frame = frame #nom de le fenêtre tkinter
-        #creation d'un bouton, on utilise highlightbackground à lla place de background car ça ne marche pas sur macos 
+        #creation d'un bouton, on utilise highlightbackground à la place de background car ça ne marche pas sur macos 
         self.button = Button(self.frame,text = str(self.name),command = self.update_place,highlightbackground='green',background = 'green')
         #on met à jour le nombre de place libre et maximal à la création de chaque place de parking
         place_libre +=1 
@@ -60,7 +67,7 @@ class Parking :
             self.change_color()
 
         #place de parking prise, on essaye de sortir 
-        elif self.button["highlightbackground"]=="red": #and place_occupe != 0 :
+        elif self.button["highlightbackground"]=="red": 
             place_libre +=1
             place_occupe -=1
             print("Nombre de place Libre = {}, nombre de place occupée occupé = {}".format(place_libre,place_occupe))
@@ -72,6 +79,21 @@ class Parking :
         place_libre_var.set("Nombre de place dispo dans le parking : "+str(place_libre)+"/"+str(place_max))
         place_occupe_var.set("Nombre de place occupé dans le parking : "+str(place_occupe)+"/"+str(place_max))
 
+
+##############################################################################################################################
+# Déclaration de fonction                                                                                                    #
+##############################################################################################################################
+
+#fonction pour ajouter des boutons/places de parking dans la fenêtre tkinter
+#frame = nom de la fenêtre tkinter; x/y_base = coordonnées du premier élément, allee_num = numéro de l'allée, num_max = nb de place par allée
+def add_places(frame,x_base,y_base,allee_num,num_max):
+    allee = Label(frame,text = f"Allé {allee_num}").place(x = x_base,y=y_base)
+    for k in range(1,num_max+1):
+        spot = Parking(fenetre,f"place {str(k)}",(x_base+(k-1)*55),y_base+20)
+        spot.add()
+    return True
+
+
 ##############################################################################################################################
 # Partie graphique                                                                                                           #
 ##############################################################################################################################
@@ -79,22 +101,32 @@ class Parking :
 
 #################################### initialisation de la fenêtre  ############################################################
 fenetre = Tk()
+#nom de la fenêtre tkinter
 fenetre.title("Parking Manager")
+#dimension de départ de la fenêtre tkinter
 fenetre.geometry("500x500")
 
 #################################### Initialisation des places de parking ######################################################
-#premier étage
-etage_1 = Label(fenetre,text = "Etage n°1") .place(x=200,y = 50)
-alle_1_1 = Label(fenetre,text = "Allé 1").place(x = 0,y=70)
-p1 = Parking(fenetre,"place 1 ",0,90)
-p1.add()
-p2 = Parking(fenetre,"place 2 ",55,90)
-p2.add()
-p3 = Parking(fenetre,"place 3 ",110,90)
-p3.add()
-for k in range(0,5):
-    spot = Parking(fenetre,f"place {str(4+k)}",(165+k*55),90)
-    spot.add() 
+#premier niveau
+etage_1 = Label(fenetre,text = "Niveau n°0") .place(x=200,y = 50)
+#allée n°1
+add_places(frame=fenetre,x_base=0, y_base=70,allee_num = 1,num_max=5)
+#allée n°2
+add_places(frame=fenetre,x_base=0, y_base=120,allee_num = 2,num_max=4)
+
+#deuxième niveau  
+etage_2 = Label(fenetre,text = "Niveau n°1") .place(x=200,y = 180)
+#allée n°1
+add_places(frame=fenetre,x_base=0, y_base=190,allee_num = 1,num_max=5)
+#allée n°2
+add_places(frame=fenetre,x_base=0, y_base=240,allee_num = 2,num_max=4)
+
+#troisième niveau  
+etage_3 = Label(fenetre,text = "Niveau n°2") .place(x=200,y = 310)
+#allée n°1
+add_places(frame=fenetre,x_base=0, y_base=320,allee_num = 1,num_max=5)
+#allée n°2
+add_places(frame=fenetre,x_base=0, y_base=370,allee_num = 2,num_max=4)
 
 #################################### Initialisation des compteur de place ######################################################
 place_libre_var = StringVar()
@@ -106,4 +138,7 @@ place_occupe_var.set("Nombre de place occupé dans le parking     : "+str(place_
 place_libre_label = Label(fenetre,textvariable = place_libre_var).place(x=0,y = 0)
 place_occupe_label = Label(fenetre,textvariable = place_occupe_var).place(x=0,y = 20)
 
+#bouton pour fermer la fenêtre tkinter
+fermer = Button(fenetre,text = "quitter" ,command = fenetre.quit,highlightbackground='blue',background = 'blue')
+fermer.place(x=200,y=460)
 fenetre.mainloop()
